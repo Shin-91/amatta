ad1dbce8ff29157c41eeb5f380d72ac258abf997
vjerksen <md040207@naver.com>
2018-06-30 20:03:32 +0900

    create package

--- app/src/main/java/com/uos/computer/amatta/MainActivity.java
+++ app/src/main/java/com/uos/computer/amatta/MainActivity.java
@@ -1,16 +1,19 @@
 package com.uos.computer.amatta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TEST HOON
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setContentView(R.layout.test);
     }
+
+
 }
