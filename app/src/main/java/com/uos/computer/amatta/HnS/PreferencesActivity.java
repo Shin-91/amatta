eaff04d7cf5c70b4ff4d2271d892265444a73613
vjerksen <md040207@naver.com>
2018-06-30 20:44:27 +0900

    환경 설정 기본 형태

---
+++ app/src/main/java/com/uos/computer/amatta/HnS/PreferencesActivity.java
@@ -0,0 +1,28 @@
+package com.uos.computer.amatta.HnS;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.uos.computer.amatta.R;
+
+public class PreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
+
+    public void OnButton1Clicked(View v){   // 알람 설정으로 들어간다.
+
+    }
+
+    public void OnButton2Clicked(View v){   // 상태 설정으로 들어간다.
+
+    }
+
+    public void OnButton3Clicked(View v){   // 리스트 화면으로 돌아간다.
+
+    }
+}
